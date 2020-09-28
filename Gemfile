40fc0d597e4cd75dce9943d39fa5612d65773788
Ranko Liang <rankoliang@gmail.com>
2020-09-28 00:27:34 -0400

    style forms with bootstrap

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'bootstrap', '~> 4.5', '>= 4.5.2'
+
 gem 'devise', '~> 4.7.3'
 
 gem 'omniauth-discord', '~> 0.1.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get '/sign_in', to: 'devise/sessions#new'
-    get '/sign_up', to: 'devise/registrations#new'
-    delete '/sign_out', to: 'devise/sessions#destroy'
-  end
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'static_pages#index'
