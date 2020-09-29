261ad4043c9e63cb84288d4a6c9640b64d0423b6
Ranko Liang <rankoliang@gmail.com>
2020-09-28 22:45:21 -0400

    style user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root 'static_pages#index'
 
-  resources :users
+  resources :users, except: %i[new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
