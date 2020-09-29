b7e043c2d5769842bb96b891226597c41ceb76e1
Ranko Liang <rankoliang@gmail.com>
2020-09-29 14:28:33 -0400

    add user show request spec

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -9,10 +9,33 @@ RSpec.describe 'Users', type: :request do
       end
     end
     context 'when logged in' do
-      let(:user) { FactoryBot.create(:user) }
+      before do
+        sign_in FactoryBot.create(:user)
+      end
+      it 'returns http success' do
+        get '/users/index'
+
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+  describe 'GET /show' do
+    let(:user) { FactoryBot.create(:user) }
+
+    context 'when not logged in' do
+      it 'redirects to new user session path' do
+        get user_path(user)
+
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'when logged in' do
       it 'returns http success' do
         sign_in user
-        get '/users/index'
+
+        get user_path(user)
+
         expect(response).to have_http_status(:success)
       end
     end
