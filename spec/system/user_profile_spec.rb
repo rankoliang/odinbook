51ed24fcb1bd68c673d8c7a82c15e4d9e0373cd1
Ranko Liang <rankoliang@gmail.com>
2020-10-09 00:07:01 -0400

    add initial manage friend buttons to profiles

--- spec/system/user_profile_spec.rb
+++ spec/system/user_profile_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe 'UserProfile', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:user) { FactoryBot.create(:user) }
+  include_context 'users'
 
   it_behaves_like 'a users resource' do
     let(:visiting_url) { user_url(user) }
   end
+
+  let(:profile) { find('.profile') }
 
   context 'when a user is signed in' do
     context 'when a user visits their own profile' do
@@ -17,7 +19,6 @@ RSpec.describe 'UserProfile', type: :system do
         sign_in user
         visit user_url(user)
       end
-      let(:profile) { find('.profile') }
       it 'renders their display name' do
         expect(profile).to have_content(user.name)
       end
@@ -31,7 +32,73 @@ RSpec.describe 'UserProfile', type: :system do
       end
 
       it 'renders the number of friends' do
-        expect(profile).to have_content pluralize(user.friends.count, 'friend') 
+        expect(profile).to have_content pluralize(user.friends.count, 'friend')
+      end
+
+      it 'does not have an add friend button' do
+        expect(profile).to have_no_button 'Add friend'
+      end
+
+      it 'does not have a remove friend button' do
+        expect(profile).to have_no_button 'Add friend'
+      end
+
+      it 'does has an edit profile button' do
+        expect(profile).to have_link href: edit_user_path(user)
+      end
+    end
+
+    context "when a user visits another user's profile" do
+      before do
+        sign_in user
+        visit user_url(other_user)
+      end
+
+      context 'when the send request button is clicked' do
+        it 'sends a friend request' do
+          click_button 'Send request'
+
+          expect(profile).to have_button 'Cancel Request'
+        end
+      end
+
+      context 'when the cancel request button is clicked' do
+        it 'cancels the request' do
+          user.request_to_be_friends(other_user)
+          click_button 'Send request'
+
+          expect(profile).to have_button 'Cancel Request'
+        end
+      end
+
+      context 'when the remove friend button is clicked' do
+        it 'removes the friend' do
+          other_user.add_friend(user)
+          visit user_url(other_user)
+
+          accept_confirm do
+            click_button 'Remove friend'
+          end
+
+          expect(profile).to have_button 'Send request'
+        end
+      end
+
+      context 'when there is a friend request pending' do
+        context 'when the add friend button is clicked' do
+          it 'adds the friend' do
+            other_user.request_to_be_friends(user)
+            visit user_url(other_user)
+
+            click_button 'Add friend'
+
+            expect(profile).to have_button 'Remove friend'
+          end
+        end
+      end
+
+      it 'does not have an edit profile button' do
+        expect(profile).to have_no_link href: edit_user_path(other_user)
       end
     end
   end
